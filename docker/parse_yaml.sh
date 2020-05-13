74486300ea15ab4bfcf14d11c9a85a4e866af210
RaymondKirk <ray.tunstill@live.co.uk>
2020-05-13 17:57:59 +0100

    Swap strategy to just escape special characters
    Trust that users won't abuse the unified api, change later to parse everything before

--- docker/parse_yaml.sh
+++ docker/parse_yaml.sh
@@ -8,14 +8,19 @@ import yaml, sys, os
 file_path, prefix, sep = sys.argv[1], sys.argv[2], sys.argv[3]
 if not os.path.isfile(file_path):
     raise IOError("'{}' is not a valid file".format(file_path))
-_invalid = ["$", "\\"]
+# These characters need to be escaped in when these vars are evaluated (`~!#$&)
+_invalid = ["$", "\\", "`", "!", "(", ")", "#", "&", "*", "&", "\t", "[", "]", "{", "}", "|", ";", "'", '"', "\n",
+            "<", ">", "?"]
 def rec_print_dict(d, p=""):
     for k, v in d.items():
         if isinstance(v, dict):
             rec_print_dict(v, p+k+sep)
         else:
-            if isinstance(v, str) and any(c in _invalid for c in v):
-                raise ValueError("Cannot have '{}' symbols in '{}' variable with value '{}'".format(', '.join(_invalid), k, v))
+            v = str(v)
+#            if any(c in _invalid for c in v):
+#                raise ValueError("Warning in file '{}' the '{}'='{}' variable has '{}' symbols in which can be dangerous".format(file_path, k, v, ', '.join(_invalid)))
+            for i in _invalid:
+                v = v.replace(i, "\\" + i)
             print('{}="{}"'.format(prefix+sep+p+k, v))
 with open(file_path, "r") as f:
     doc = yaml.load(f)
