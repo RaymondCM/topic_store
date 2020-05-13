0ac02aa96dc85beda09eda4202ec8c2100759c9d
RaymondKirk <ray.tunstill@live.co.uk>
2020-05-13 17:33:28 +0100

    Updated parser so spaces are supported in variables

--- docker/parse_yaml.sh
+++ docker/parse_yaml.sh
@@ -2,32 +2,34 @@
 #  Raymond Kirk (Tunstill) Copyright (c) 2020
 #  Email: ray.tunstill@gmail.com
 
-# Inspired by: https://github.com/mrbaseman/parse_yaml.git (changed since the git version hangs)
+# Python function to load yaml and print to STD_OUT
+_get_yaml_python=$(cat <<'EOF'
+import yaml, sys, os
+file_path, prefix, sep = sys.argv[1], sys.argv[2], sys.argv[3]
+if not os.path.isfile(file_path):
+    raise IOError("'{}' is not a valid file".format(file_path))
+_invalid = ["$", "\\"]
+def rec_print_dict(d, p=""):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            rec_print_dict(v, p+k+sep)
+        else:
+            if isinstance(v, str) and any(c in _invalid for c in v):
+                raise ValueError("Cannot have '{}' symbols in '{}' variable with value '{}'".format(', '.join(_invalid), k, v))
+            print('{}="{}"'.format(prefix+sep+p+k, v))
+with open(file_path, "r") as f:
+    doc = yaml.load(f)
+    rec_print_dict(doc)
+EOF
+)
 
-function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*'
-   local w='[a-zA-Z0-9_]*'
-   local fs=$(echo @ | tr @ '\034')
-   sed -ne "s|,$s\]$s\$|]|" \
-        -e ":1;s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: [\3]\n\1  - \4|;t1" \
-        -e "s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s\]|\1\2:\n\1  - \3|;p" $1 | \
-   sed -ne "s|,$s}$s\$|}|" \
-        -e ":1;s|^\($s\)-$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1- {\2}\n\1  \3: \4|;t1" \
-        -e    "s|^\($s\)-$s{$s\(.*\)$s}|\1-\n\1  \2|;p" | \
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)-${s}[\"']\(.*\)[\"']$s\$|\1$fs$fs\2|p" \
-        -e "s|^\($s\)-$s\(.*\)$s\$|\1$fs$fs\2|p" \
-        -e "s|^\($s\)\($w\)$s:${s}[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" | \
-   awk -F"${fs}" '{
-      indent = length($1)/2;
-      yaml_var_name[indent] = $2;
-      for (i in yaml_var_name) {if (i > indent) {delete yaml_var_name[i]; idx[i]=0}}
-      if(length($2)== 0){  yaml_var_name[indent]= ++idx[indent] };
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) { vn=(vn)(yaml_var_name[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'"${prefix}"'",vn, yaml_var_name[indent], $3);
-      }
-   }'
+# Usage:
+#   To print: parse_yaml "$file_path" "$prefix" "$sep"
+#   As variables: eval $(parse_yaml "$file_path" "$prefix" "$sep")
+function parse_yaml() {
+    file_path=${1}
+    prefix_str=${2:-""}
+    sep_str=${3:-"_"}
+    local fs=$(echo @ | tr @ '\034')
+    echo "$(python -c "$_get_yaml_python" "${file_path}" "${prefix_str}" "${sep_str}")"
 }
