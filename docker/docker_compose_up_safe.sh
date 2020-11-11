1abf1fff0070a44be5ba8fd88dc3bd91fea155f6
RaymondKirk <ray.tunstill@live.co.uk>
2020-11-11 16:17:57 +0000

    Added warning message for UID > 1000

--- docker/docker_compose_up_safe.sh
+++ docker/docker_compose_up_safe.sh
@@ -109,5 +109,16 @@ trap cleanup INT TERM EXIT
 # Bring container down if already up (avoid corruption)
 docker-compose down
 
+# Try to launch docker with user UID:GID
+TS_UID_GID=1000:1000
+if [[ ! "$(id -u):$(id -g)" = "1000:1000" ]]; then
+  echo -e "You're about to launch docker mongodb with user id ($(id -u)) group id ($(id -g))"
+  read -r -p "Would you like to continue (no will use .env default UID:GID 1000:1000)? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+      TS_UID_GID=$(id -u):$(id -g)
+  fi
+fi
+export TS_UID_GID=$TS_UID_GID
+
 # Bring container up and block execution
-TS_UID_GID=$(id -u):$(id -g) docker-compose up
+docker-compose up
