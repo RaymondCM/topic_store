c4490e821efe74fd21faa92ac21710d142ab9ddb
RaymondKirk <ray.tunstill@live.co.uk>
2021-02-03 16:01:32 +0000

    Don't break if no ros install on machine

--- src/topic_store/__init__.py
+++ src/topic_store/__init__.py
@@ -2,11 +2,15 @@
 #  Email: ray.tunstill@gmail.com
 
 from .api import load
+from .data import TopicStore
 
 
 def get_package_root(use_rospkg=True):
     import pathlib
     if use_rospkg:
-        import rospkg
-        return pathlib.Path(rospkg.RosPack().get_path("topic_store"))
+        try:
+            import rospkg
+            return pathlib.Path(rospkg.RosPack().get_path("topic_store"))
+        except:
+            pass  # use default way of finding package
     return (pathlib.Path(__file__) / "../../..").resolve()
