a0b1ea6c95efc515a2de15ee6f479ea794a59786
RaymondKirk <ray.tunstill@live.co.uk>
2021-02-12 19:49:27 +0000

    Overhaul of sanitation methods
    Previous implementation was unreliable and had an issue with objects in different parts of the nested dict would be magically replaced randomly due to objects in python being created with the same id. Some unavoidable copy by reference errors in last implementation, now all are copied by value. Note it is still possible to mess this up by running convert dictionary functions on the same dict objects at different times.

--- src/topic_store/sanitation.py
+++ src/topic_store/sanitation.py
@@ -11,19 +11,18 @@ import rospy
 import roslib.message
 
 try:
-    from collections.abc import Mapping, Sequence, Set, ItemsView, Iterable
-except ImportError:
-    from collections import Mapping, Sequence, Set, ItemsView, Iterable
+    from collections.abc import Mapping, Sequence, Set, ItemsView, Iterable, Callable
+except ImportError:  # python3 so use collections package
+    from collections import Mapping, Sequence, Set, ItemsView, Iterable, Callable
 
 try:
     unicode
 except NameError:  # python3 so use unicode=str
     unicode = str
 
-
 from topic_store.utils import ros_time_as_ms
 
-__all__ = ["MongoDBParser", "DefaultTypeParser", "MongoDBReverseParser", "sanitise_dict", "desanitise_dict"]
+__all__ = ["MongoDBParser", "DefaultTypeParser", "MongoDBReverseParser", "sanitise_dict", "rosify_dict"]
 
 
 def idx_of_instance(obj, instance_checks):
@@ -35,8 +34,9 @@ def idx_of_instance(obj, instance_checks):
     return -1
 
 
+# TODO: Integrate this type coercion and DictConverter together
 class DefaultTypeParser:
-    """Type coercion utility class.
+    """Type coercion utility class. Useful for extra quick type conversions for db/filesystem.
 
         Examples:
         >>> parser = DefaultTypeParser()
@@ -50,8 +50,7 @@ class DefaultTypeParser:
         self._core_types = [dict, list, tuple, set]
         self._type_converters = {t: self.__parse_dict if t is dict else self.__parse_list for t in self._core_types}
         self._instance_converters = ()
-        self.add_converters({genpy.Message: sanitise_dict, genpy.Time: sanitise_dict,
-                             genpy.Duration: sanitise_dict}, instance=True)
+        self.add_converters({}, instance=True)
 
     def add_converters(self, type_to_converter_map, instance=False, replace_existing=True):
         """Adds more conversion functions. Must pass a dict of type: converter function pairs.
@@ -139,177 +138,235 @@ class MongoDBReverseParser(DefaultTypeParser):
     #     return str(s)
 
 
-class ROSItemsView(ItemsView):
-    def __init__(self, mapping, *args, **kwargs):
-        super(ROSItemsView, self).__init__(mapping, *args, **kwargs)
-        if not isinstance(self._mapping, (genpy.Message, genpy.Time, genpy.Duration)):
-            raise ValueError("Only genpy.Message types can be used with ROSItemsView")
+class DictConverter:
+    __unique_id = object()
 
-        # Attempt to extract the message type for bi-directional conversion
-        self.msg_type = getattr(self._mapping, "_type", None)
-        if self.msg_type is None:
-            if issubclass(type(self._mapping), genpy.Time):
-                self.msg_type = "genpy.Time"
-            elif issubclass(type(self._mapping), genpy.Duration):
-                self.msg_type = "genpy.Duration"
+    def __init__(self, enter_fn=None, exit_fn=None, visit_fn=None):
+        if enter_fn is None:
+            enter_fn = self.default_enter_fn
+        if exit_fn is None:
+            exit_fn = self.default_exit_fn
+        if not isinstance(enter_fn, Callable):
+            raise TypeError(
+                "Enter function must be callable of signature func(parents, key, value) "
+                "-> (empty new object, iterable of items to fill empty new object)"
+            )
+        if not isinstance(exit_fn, Callable):
+            raise TypeError(
+                "Exit function must be callable of signature func(parents, key, old_object, new_object, new_items) "
+                "-> (populated new object)"
+            )
+        if visit_fn is not None and not isinstance(visit_fn, Callable):
+            raise TypeError(
+                "Visit function must be callable of signature func(parents, key, value) "
+                "-> (new_key, new_value)"
+            )
+        self._enter_fn = enter_fn
+        self._exit_fn = exit_fn
+        self._visit_fn = visit_fn
 
-        # If data is a ROS message then it has to be serialised to python
-        self.slots = [k for k in self._mapping.__slots__]
-        self.connection_header = getattr(self._mapping, "_connection_header", None)
+    def convert(self, data_dict):
+        return self(data_dict)
+
+    def __call__(self, data_dict):
+        if not isinstance(data_dict, dict):
+            raise TypeError('Expected dictionary type, not: {}'.format(type(data_dict)))
+
+        parents, seen_ids, stack = (), {}, [(None, data_dict)]
+        new_items_stack = []
+        value = None
+
+        while stack:
+            key, value = stack.pop()
+            id_value = id(value)
+            if key is self.__unique_id:
+                key, new_object, old_object = value
+                id_value = id(old_object)
+                parents, new_items = new_items_stack.pop()
+                value = self._exit_fn(parents, key, old_object, new_object, new_items)
+                seen_ids[id_value] = value
+                if not new_items_stack:
+                    continue
+            elif id_value in seen_ids:
+                value = seen_ids[id_value]
+            else:
+                new_object, new_items = self._enter_fn(parents, key, value)
+                if new_items is not False:
+                    seen_ids[id_value] = new_object
+                    new_items_stack.append((parents, []))
+                    if value is not data_dict:
+                        parents += (key,)
+                    # Use self.__unique_id objects id to specify when this object is done
+                    stack.append((self.__unique_id, (key, new_object, value)))
+                    if bool(new_items):
+                        stack.extend(reversed(list(new_items)))
+                    continue
+
+            # Visit the item (returns key, value) (or just return current item if no visit supplied)
+            if self._visit_fn is None:
+                visited_item = (key, value)
+            else:
+                visited_item = self._visit_fn(parents, key, value)
+
+            try:
+                new_items_stack[-1][1].append(visited_item)
+            except IndexError:
+                raise TypeError('Expected root dictionary, not: {}'.format(data_dict))
+
+        return value
+
+    @staticmethod
+    def default_enter_fn(parents, key, value):
+        # Responsible for getting the items to be traversed/updated in visit and exit respectively
+        if isinstance(value, basestring):
+            return value, False
+        elif isinstance(value, Mapping):
+            return value.__class__(), ItemsView(value)
+        elif isinstance(value, Sequence):
+            return value.__class__(), enumerate(value)
+        elif isinstance(value, Set):
+            return value.__class__(), enumerate(value)
+        else:
+            return value, False
+
+    @staticmethod
+    def default_visit_fn(parents, key, value):
+        # Responsible for remapping the key, value pairs
+        return key, value
+
+    @staticmethod
+    def default_exit_fn(parents, key, old_object, new_object, new_items):
+        # Responsible for updating the shell new_object item with new_items values
+        ret = new_object
+        if isinstance(new_object, Mapping):
+            ret.update(new_items)
+        elif isinstance(new_object, (Sequence, Set)):
+            values = [v for i, v in new_items]
+            try:
+                if isinstance(new_object, Set):
+                    ret.update(values)
+                else:
+                    ret.extend(values)
+            except AttributeError:
+                ret = ret.__class__(values)
+        else:
+            raise RuntimeError('Unexpected iterable: {}'.format(type(new_object)))
+        return ret
+
+
+class ROSItemsView:
+    def __init__(self, mapping):
+        if not isinstance(mapping, (dict, genpy.Message, genpy.Time, genpy.Duration)):
+            raise ValueError("Only dict, genpy.Message, genpy.Time, genpy.Duration types can be used with ROSItemsView")
+
+        if isinstance(mapping, dict):
+            # If data is a dict then it already contains the iterable items
+            self.data = mapping
+        else:
+            # Otherwise must be a ROS type so create iterable using slots and getattr
+            slots = mapping.__slots__
+            if issubclass(type(mapping), (genpy.Time, genpy.Duration)):
+                slots = genpy.Time.__slots__  # rospy.[Time|Duration] have empty slots
+            self.data = {k: getattr(mapping, k) for k in slots}
+
+        self.len = len(self.data)  # length minus the next meta values
 
     def __len__(self):
-        return len(self.slots)
+        return self.len
 
     def __contains__(self, item):
         key, value = item
-        return key in self.slots
+        return key in self.data
 
     def __iter__(self):
-        # When iterating over the slots use getattr & slots to turn genpy.Message into iterables
-        for key in self.slots:
-            yield key, getattr(self._mapping, key)
-        # Finally return a time stamped ros meta message to allow conversion back to a message type
-        yield "_ros_meta", {'time': ros_time_as_ms(), 'type': self.msg_type}
-        # Preserve connection header for ROSBag conversion support
-        if self.connection_header:
-            yield "_connection_header", self.connection_header
+        for key, value in self.data.items():
+            yield key, value
 
 
-def default_enter_fn(path, key, value):
-    if isinstance(value, basestring):
-        return value, False
-    elif isinstance(value, Mapping):
-        return value.__class__(), ItemsView(value)
-    elif isinstance(value, Sequence):
-        return value.__class__(), enumerate(value)
-    elif isinstance(value, Set):
-        return value.__class__(), enumerate(value)
+def get_message_type(ros_item):
+    msg_type = getattr(ros_item, "_type", None)
+    if msg_type is None:
+        if all([hasattr(ros_item, x) for x in ["secs", "nsecs"]]):
+            if issubclass(type(ros_item), genpy.Time):
+                msg_type = "genpy.Time"
+            elif issubclass(type(ros_item), genpy.Duration):
+                msg_type = "genpy.Duration"
+        elif isinstance(ros_item, dict) and "_ros_meta" in ros_item:
+            msg_type = ros_item["_ros_meta"]["type"]
+    if msg_type is None:
+        raise TypeError("Could not get type of {}".format(ros_item))
+    return msg_type
+
+
+def get_ros_meta(ros_item):
+    _type = get_message_type(ros_item)
+    _ros_meta = {'_ros_meta': {'time': ros_time_as_ms(), 'type': _type}}
+    connection_header = getattr(ros_item, "_connection_header", None)
+    if connection_header:
+        _ros_meta["_connection_header"] = connection_header
+    return _ros_meta
+
+
+def ros_msg_from_string(msg_cls):
+    if msg_cls in ["genpy.Time", "genpy.Duration"]:
+        cls_type = rospy.rostime.Time if ".Time" in msg_cls else rospy.rostime.Duration
     else:
-        return value, False
+        cls_type = roslib.message.get_message_class(msg_cls)
+        if not cls_type:
+            raise rospy.ROSException("Cannot load message class for [{}].  Please ensure the relevant package "
+                                     "is installed on your system.".format(msg_cls))
+    return cls_type
 
 
-def default_exit_fn(path, key, old_parent, new_parent, new_items):
-    if isinstance(new_parent, Mapping):
-        new_parent.update(new_items)
-    elif isinstance(new_parent, (Sequence, Set)):
-        values = [v for i, v in new_items]
-        try:
-            if isinstance(new_parent, Set):
-                new_parent.update(values)
-            else:
-                new_parent.extend(values)
-        except AttributeError:
-            new_parent = new_parent.__class__(values)
-    else:
-        raise RuntimeError('Unexpected iterable: {}'.format(type(new_parent)))
-    return new_parent
-
-
-def enter_fn_ros_to_dict(path, key, value):
+def enter_fn_ros_to_dict(parents, key, value):
     if isinstance(value, (genpy.Message, genpy.Time, genpy.Duration)):
-        # genpy.Message return empty class shell as dict and ROSItemsView (slot, len and iterable support)
-        return value.__class__(), ROSItemsView(value)
+        # genpy.Message return empty dict to add ROSItemsView values to (slot, len and iterable support)
+        return dict(), ROSItemsView(value)
     else:
-        return default_enter_fn(path, key, value)
+        return DictConverter.default_enter_fn(parents, key, value)
 
 
-def exit_fn_ros_to_dict(path, key, old_parent, new_parent, new_items):
-    if isinstance(new_parent, (genpy.Message, genpy.Time, genpy.Duration)):
-        # New parent is dict of genpy.Message slots (new_items from ROSItemsView)
-        return dict(new_items)
+def exit_fn_ros_to_dict(parents, key, old_object, new_object, new_items):
+    ret = new_object
+    if isinstance(new_object, Mapping):
+        # New parent is empty dict, fill with genpy.Message slots (new_items from ROSItemsView)
+        ret.update(new_items)
+        if isinstance(old_object, (genpy.Message, genpy.Time, genpy.Duration)):
+            # Add ros meta to new item for conversion back to ros types
+            ret.update(get_ros_meta(old_object))
     else:
-        return default_exit_fn(path, key, old_parent, new_parent, new_items)
+        ret = DictConverter.default_exit_fn(parents, key, old_object, new_object, new_items)
+    return ret
 
 
-def enter_fn_dict_to_ros(path, key, value):
+def enter_fn_dict_to_ros(parents, key, value):
+    # If this was a ROS message
     if isinstance(value, Mapping) and "_ros_meta" in value:
         # genpy.Message return empty class shell as dict and ROSItemsView (slot, len and iterable support)
-        msg_type = value["_ros_meta"]["type"]
-        cls_type = None
-        if msg_type in ["genpy.Time", "genpy.Duration"] and all(s in value for s in ["secs", "nsecs"]):
-            cls_type = rospy.rostime.Time if "Time" in msg_type else rospy.rostime.Duration
-        else:
-            cls_type = roslib.message.get_message_class(msg_type)
-            if not cls_type:
-                raise rospy.ROSException("Cannot load message class for [{}].  Please ensure the relevant package "
-                                         "is installed on your system.".format(msg_type))
-        # del value["_ros_meta"]
-        return cls_type(), ItemsView(value)
+        cls_type = ros_msg_from_string(value["_ros_meta"]["type"])
+        return cls_type(), ROSItemsView(value)
     else:
-        return default_enter_fn(path, key, value)
+        return DictConverter.default_enter_fn(parents, key, value)
 
 
-def exit_fn_dict_to_ros(path, key, old_parent, new_parent, new_items):
-    if isinstance(new_parent, (genpy.Message, genpy.Time, genpy.Duration)):
+def exit_fn_dict_to_ros(parents, key, old_object, new_object, new_items):
+    if isinstance(new_object, (genpy.Message, genpy.Time, genpy.Duration)):
         # New parent is dict of genpy.Message slots (new_items from ROSItemsView)
         for attribute_key, attribute_value in new_items:  # or cls = msg_class(**v) after removing ros meta etc
             if attribute_key == "_ros_meta":
                 continue
             try:
-                setattr(new_parent, attribute_key, attribute_value)
+                setattr(new_object, attribute_key, attribute_value)
             except KeyError as e:
                 # Here we accept that if the message type has changed that we cannot necessarily fill all slots
                 rospy.logwarn("Could not set slot '{}' for class '{}' maybe the message definitions are "
-                              "incompatible".format(attribute_key, new_parent))
-        return new_parent
+                              "incompatible".format(attribute_key, new_object))
+        return new_object
     else:
-        return default_exit_fn(path, key, old_parent, new_parent, new_items)
+        return DictConverter.default_exit_fn(parents, key, old_object, new_object, new_items)
 
 
-def convert_dictionary(data_dict, enter_fn, exit_fn, visit_fn=None):
-    path, registry, stack = (), {}, [(None, data_dict)]
-    new_items_stack = []
-    value = None
-    exit_operation = object()
-
-    while stack:
-        key, value = stack.pop()
-        id_value = id(value)
-        if key is exit_operation:
-            key, new_parent, old_parent = value
-            id_value = id(old_parent)
-            path, new_items = new_items_stack.pop()
-            value = exit_fn(path, key, old_parent, new_parent, new_items)
-            registry[id_value] = value
-            if not new_items_stack:
-                continue
-        elif id_value in registry:
-            value = registry[id_value]
-        else:
-            res = enter_fn(path, key, value)
-            new_parent, new_items = res
-            if new_items is not False:
-                # traverse unless False is explicitly passed
-                registry[id_value] = new_parent
-                new_items_stack.append((path, []))
-                if value is not data_dict:
-                    path += (key,)
-                stack.append((exit_operation, (key, new_parent, value)))
-                if bool(new_items):
-                    stack.extend(reversed(list(new_items)))
-                continue
-        if visit_fn is not None:
-            try:
-                visited_item = visit_fn(path, key, value)
-            except Exception:
-                raise
-            if visited_item is False:
-                continue  # drop
-            elif visited_item is True:
-                visited_item = (key, value)
-        else:
-            visited_item = (key, value)
-        try:
-            new_items_stack[-1][1].append(visited_item)
-        except IndexError:
-            raise TypeError('Expected root dictionary, not: {}'.format(data_dict))
-
-    return value
-
-
-def sanitise_dict(nested_dict):
-    return convert_dictionary(nested_dict, enter_fn=enter_fn_ros_to_dict, exit_fn=exit_fn_ros_to_dict)
-
-
-def desanitise_dict(nested_dict):
-    return convert_dictionary(nested_dict, enter_fn=enter_fn_dict_to_ros, exit_fn=exit_fn_dict_to_ros)
+# Creates a dictionary of python/json/bson compatible types from ROS types
+sanitise_dict = DictConverter(enter_fn=enter_fn_ros_to_dict, exit_fn=exit_fn_ros_to_dict)
+# Creates a dictionary of ROS/python compatible types from python types
+rosify_dict = DictConverter(enter_fn=enter_fn_dict_to_ros, exit_fn=exit_fn_dict_to_ros)
