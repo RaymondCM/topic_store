1b67f7e087bb75964cc260d5ccf5162b060acf0d
RaymondKirk <ray.tunstill@live.co.uk>
2021-04-27 15:17:36 +0100

    Defaults for run_scenario.py

--- scripts/run_scenario.py
+++ scripts/run_scenario.py
@@ -16,11 +16,11 @@ def run_scenario():
 
     # Get parameters
     stabilise_time = rospy.get_param('~stabilise_time', 0)
-    scenario_file = resolve_scenario_yaml(rospy.get_param('~scenario_file', "local_tests.yaml"))
+    scenario_file = resolve_scenario_yaml(rospy.get_param('~scenario_file', "database_default.yaml"))
     verbose = rospy.get_param('~verbose', True)
     queue_size = rospy.get_param('~queue_size', 100)
     threads = rospy.get_param('~threads', 1)
-    threads_auto = rospy.get_param('~threads_auto', False)
+    threads_auto = rospy.get_param('~threads_auto', True)
     use_grid_fs = rospy.get_param('~use_grid_fs', False)
 
     data_logger = ScenarioRunner(str(scenario_file), stabilise_time, verbose, queue_size, threads, threads_auto,
